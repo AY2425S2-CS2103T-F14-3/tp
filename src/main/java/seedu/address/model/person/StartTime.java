97d41c2eff1427497791d810e90b4ed6762471e0
Zekai <leonhuangzekai@hotmail.com>
2025-04-07 13:22:06 +0800

    Solve February issue

--- src/main/java/seedu/address/model/person/StartTime.java
+++ src/main/java/seedu/address/model/person/StartTime.java
@@ -71,21 +71,21 @@ public class StartTime implements Comparable<StartTime> {
 
     private static int getDaysInMonth(int year, int month) {
         switch (month) {
-            case 1: // January
-            case 3: // March
-            case 5: // May
-            case 7: // July
-            case 8: // August
-            case 10: // October
-            case 12: // December
-                return 31;
-            case 4: // April
-            case 6: // June
-            case 9: // September
-            case 11: // November
-                return 30;
-            default: // February
-                return isLeapYear(year) ? 29 : 28;
+        case 1: // January
+        case 3: // March
+        case 5: // May
+        case 7: // July
+        case 8: // August
+        case 10: // October
+        case 12: // December
+            return 31;
+        case 4: // April
+        case 6: // June
+        case 9: // September
+        case 11: // November
+            return 30;
+        default: // February
+            return isLeapYear(year) ? 29 : 28;
         }
     }
 
