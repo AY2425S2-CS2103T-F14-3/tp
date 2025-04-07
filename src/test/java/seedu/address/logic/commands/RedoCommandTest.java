a9f9c6225f492a339829fa28ca9a930d8b7f26e3
Li_Daoxin <lidaoxinldx@gmail.com>
2025-04-08 03:07:42 +0800

    Update undo and redo test

--- src/test/java/seedu/address/logic/commands/RedoCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RedoCommandTest.java
@@ -12,14 +12,17 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 
-/**
- * Contains integration tests (interaction with the Model) and unit tests for UndoCommand.
- */
-public class UndoCommandTest {
+public class RedoCommandTest {
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
     @Test
-    public void execute_undoDeleteCommand_success() {
+    public void execute_noUndoToRedo_throwsCommandException() {
+        RedoCommand redoCommand = new RedoCommand();
+        assertThrows(CommandException.class, () -> redoCommand.execute(model));
+    }
+
+    @Test
+    public void execute_redoAfterUndo_success() {
         Person firstPerson = model.getFilteredPersonList().get(6);
 
         DeleteCommand deleteCommand = new DeleteCommand(firstPerson);
@@ -30,24 +33,65 @@ public class UndoCommandTest {
         }
 
         UndoCommand undoCommand = new UndoCommand();
-
         try {
             undoCommand.execute(model);
         } catch (CommandException e) {
             e.printStackTrace();
         }
 
-        assertEquals(model.getFilteredPersonList().size(), 7);
+        RedoCommand redoCommand = new RedoCommand();
+        try {
+            redoCommand.execute(model);
+        } catch (CommandException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(model.getFilteredPersonList().size(), 6);
     }
 
     @Test
-    public void execute_noCommandToUndo_throwsCommandException() {
+    public void execute_redoMultipleUndos_success() {
+        Person firstPerson = model.getFilteredPersonList().get(6);
+        Person secondPerson = model.getFilteredPersonList().get(5);
+
+        DeleteCommand deleteFirstCommand = new DeleteCommand(firstPerson);
+        DeleteCommand deleteSecondCommand = new DeleteCommand(secondPerson);
+
+        try {
+            deleteFirstCommand.execute(model);
+            deleteSecondCommand.execute(model);
+        } catch (CommandException e) {
+            e.printStackTrace();
+        }
+
         UndoCommand undoCommand = new UndoCommand();
-        assertThrows(CommandException.class, () -> undoCommand.execute(model));
+        try {
+            undoCommand.execute(model);
+            undoCommand.execute(model);
+        } catch (CommandException e) {
+            e.printStackTrace();
+        }
+
+        RedoCommand redoCommand = new RedoCommand();
+        try {
+            redoCommand.execute(model);
+        } catch (CommandException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(model.getFilteredPersonList().size(), 6);
+
+        try {
+            redoCommand.execute(model);
+        } catch (CommandException e) {
+            e.printStackTrace();
+        }
+
+        assertEquals(model.getFilteredPersonList().size(), 5);
     }
 
     @Test
-    public void execute_undoMultipleCommands_success() {
+    public void execute_noRedoAfterModifyingCommand_throwsCommandException() {
         Person firstPerson = model.getFilteredPersonList().get(6);
         Person secondPerson = model.getFilteredPersonList().get(5);
 
@@ -69,32 +113,27 @@ public class UndoCommandTest {
             e.printStackTrace();
         }
 
-        assertEquals(model.getFilteredPersonList().size(), 6);
+        Person thirdPerson = model.getFilteredPersonList().get(4);
+        DeleteCommand deleteThirdCommand = new DeleteCommand(thirdPerson);
 
         try {
-            undoCommand.execute(model);
+            deleteThirdCommand.execute(model);
         } catch (CommandException e) {
             e.printStackTrace();
         }
 
-        assertEquals(model.getFilteredPersonList().size(), 7);
+        RedoCommand redoCommand = new RedoCommand();
+        assertThrows(CommandException.class, () -> redoCommand.execute(model));
     }
 
     @Test
-    public void execute_undoAddCommand_success() {
+    public void execute_redoNonModifyingCommand_noChange() {
         Person newPerson = model.getFilteredPersonList().get(0);
 
-        AddCommand addCommand = new AddCommand(newPerson);
         DeleteCommand deleteCommand = new DeleteCommand(newPerson);
 
         try {
             deleteCommand.execute(model);
-        } catch (CommandException e) {
-            e.printStackTrace();
-        }
-
-        try {
-            addCommand.execute(model);
         } catch (CommandException e) {
             e.printStackTrace();
         }
@@ -103,22 +142,6 @@ public class UndoCommandTest {
 
         try {
             undoCommand.execute(model);
-            undoCommand.execute(model);
-        } catch (CommandException e) {
-            e.printStackTrace();
-        }
-
-        assertEquals(model.getFilteredPersonList().get(0), newPerson);
-    }
-
-    @Test
-    public void execute_undoNonModifyingCommand_noChange() {
-        Person newPerson = model.getFilteredPersonList().get(0);
-
-        DeleteCommand deleteCommand = new DeleteCommand(newPerson);
-
-        try {
-            deleteCommand.execute(model);
         } catch (CommandException e) {
             e.printStackTrace();
         }
@@ -129,14 +152,14 @@ public class UndoCommandTest {
         SortCommand sortCommand = new SortCommand();
         sortCommand.execute(model);
 
-        UndoCommand undoCommand = new UndoCommand();
+        RedoCommand redoCommand = new RedoCommand();
 
         try {
-            undoCommand.execute(model);
+            redoCommand.execute(model);
         } catch (CommandException e) {
             e.printStackTrace();
         }
 
-        assertEquals(model.getFilteredPersonList().size(), 7);
+        assertEquals(model.getFilteredPersonList().size(), 6);
     }
 }
